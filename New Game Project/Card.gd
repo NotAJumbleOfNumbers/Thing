5788544fe70a537a854afc84fe440c98b46ff151
NotAJumbleOfNumbers <notajumbleofnumbers@gmail.com>
2023-06-25 17:52:56 -0500

    Now I'm having issues with level loading
    Yaaaaay

--- New Game Project/Card.gd
+++ New Game Project/Card.gd
@@ -2,23 +2,44 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-export var attack = 0
+export var attack = 1
+export var health = 3
+export var cost = 2
+export var description = "[center]Test description.  This bird is so silly!!![/center]"
+export var icon = preload("res://icon.png")
+
 # var b = "text"
 signal selection(node)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = String(attack)
+	$Label2.text = String(health)
+	$Label3.text = String(cost)
+	$RichTextLabel.bbcode_text = description
+	$Sprite.texture = icon
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	$Label.text = String(attack)
+	$Label2.text = String(health)
+	$Label3.text = String(cost)
+	if get_node("/root/Main").won:
+		$Sprite.rotation_degrees += 360*delta
 	var mouse_position = get_parent().get_global_mouse_position();
 	var heavy_rect = get_node("23F9").get_rect();
 	heavy_rect.position += get_node("23F9").global_position;
+	if (heavy_rect.has_point(mouse_position) or get_node("/root/Main").selection == get_node(".")) and not get_node("/root/Main").won:
+		$border.visible = true
+	else:
+		$border.visible = false
 	if heavy_rect.has_point(mouse_position):
-		if Input.is_action_just_pressed("mouse_click"):
-			emit_signal("selection", get_node("."))
+		$RichTextLabel.visible = true
+	else:
+		$RichTextLabel.visible = false
+	if Input.is_action_just_pressed("mouse_click") and heavy_rect.has_point(mouse_position):
+		emit_signal("selection", get_node("."))
 
 
 
--- New Game Project/Main.gd
+++ New Game Project/Main.gd
@@ -1,6 +1,8 @@
 extends Node
 var level = 1
-var selfection = null
+var energy = 3
+var won = false
+var selection = null
 onready var PlayerList = get_node("PlayerList");
 onready var EnemyList = get_node("EnemyList");
 onready var HandList = get_node("HandList");
@@ -9,35 +11,72 @@ onready var HandList = get_node("HandList");
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	OS.center_window();
-	connect_cards(HandList)
-	connect_cards(PlayerList)
-	connect_cards(EnemyList)
-	pass # Replace with function body.
-
+	connect_cards()
+	load_level(1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	straighten_out_cards(PlayerList)
-	straighten_out_cards(EnemyList)
-	straighten_out_cards(HandList)
-	pass
-	
+	straighten_out_cards()
+	if EnemyList.get_children().size() == 0:
+		won = true
+		selection = null
 
-func straighten_out_cards(array):
-	for i in range(array.get_children().size()):
-		var Card = array.get_children()[i];
-		Card.position.x = Card.position.x * 0.9 + (i*100-array.get_children().size()*50+50) * 0.1
-		Card.position.y = 0
-		pass
+func straighten_out_cards():
+	for array in [PlayerList, EnemyList, HandList]:
+		for i in range(array.get_children().size()):
+			var Card = array.get_children()[i];
+			Card.position.x = Card.position.x * 0.9 + (i*100-array.get_children().size()*50+50) * 0.1
+			Card.position.y = Card.position.y * 0.9
 
-
-func connect_cards(array):
-	for i in range(array.get_children().size()):
-		array.get_children()[i].connect("selection", self, "_on_Card_selection")
-		pass
-
+func connect_cards():
+	for array in [PlayerList, EnemyList, HandList]:
+		for i in range(array.get_children().size()):
+			array.get_children()[i].connect("selection", self, "_on_Card_selection")
 
 func _on_Card_selection(node):
 	var card = node
-	card.queue_free()
-	pass # Replace with function body.
+	if selection == null:
+		selection = card
+	else:
+		perform_action(selection, card)
+		selection = null
+
+func perform_action(first_card, second_card):
+	if first_card == second_card:
+		if first_card.get_parent() == get_node("HandList"):
+			play(first_card)
+	if first_card.get_parent() == get_node("PlayerList") and second_card.get_parent() == get_node("EnemyList"):
+		fight(first_card, second_card)
+
+func fight(first_card, second_card):
+	second_card.health -= first_card.attack
+	first_card.health -= second_card.attack
+	cleanup()
+
+func cleanup():
+	for array in [PlayerList, EnemyList, HandList]:
+		for i in range(array.get_children().size()):
+			if array.get_children()[i].health <= 0:
+				array.get_children()[i].queue_free()
+	connect_cards()
+
+func play(card):
+	if energy >= card.cost:
+		energy -= card.cost
+		card.get_parent().remove_child(card)
+		PlayerList.add_child(card)
+
+# warning-ignore:unused_argument
+func load_level(number):
+	for array in [PlayerList, EnemyList, HandList]:
+		for i in range(array.get_children().size()):
+				array.get_children()[i].queue_free()
+	var loadinglevel : PackedScene = load("res://levels/level1.tscn")
+# warning-ignore:unused_variable
+	var loadedlevel = loadinglevel.instance()
+	add_child(loadedlevel)
+	won = false
+	for array in [PlayerList, EnemyList, HandList]:
+		for i in range($Level.array.get_children().size()):
+				array.add_child($Level.array.get_children()[i])
+	pass
